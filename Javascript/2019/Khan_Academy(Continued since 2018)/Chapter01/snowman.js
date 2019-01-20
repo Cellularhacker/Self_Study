2914b8381e8937c93b7e4e0d5384bce189c31dab
Dongwon Lee <wva3cdae@gmail.com>
2019-01-21 01:49:50 +0900

    Edited comment

---
+++ Javascript/2019/Khan_Academy(Continued since 2018)/Chapter01/snowman.js
@@ -0,0 +1,22 @@
+// Using Processing.js
+/*
+* Date: 2018-04-29 12:55 KST
+* Modified: 2019-01-20 14:42 KST
+* Writer: Cellularhacker
+* Project: SELF_STUDY/Javascript/2019/Khan_Academy
+* Chapter: No.1
+* filename: snowman.js
+* License: GNU General Public License v3.0
+*/
+
+// Ground
+rect (0, 363, 399, 35); //floor
+
+// Bodies
+ellipse(200, 300, 150, 150); //bottom of body
+ellipse(200, 200, 100, 100); //middle of body
+ellipse(200, 120, 75, 75); //top of body
+
+// Arms
+line(160, 200, 80, 120); //left arm
+line(240, 200, 320, 120); //right arm
