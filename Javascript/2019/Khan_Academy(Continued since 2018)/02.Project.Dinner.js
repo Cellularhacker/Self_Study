077d5e61466aed9ddb4deedc17e664a2497f13ad
Dongwon Lee <wva3cdae@gmail.com>
2019-01-21 03:04:09 +0900

    Added Chaper02's Project.

---
+++ Javascript/2019/Khan_Academy(Continued since 2018)/02.Project.Dinner.js
@@ -0,0 +1,29 @@
+// Using Processing.js
+/*
+* Date: 2019-01-21 03:03 KST
+* Writer: Cellularhacker
+* Project: SELF_STUDY/Javascript/2019/Khan_Academy
+* Chapter: No.2
+* filename: 02.Project.Dinner.js
+* License: GNU General Public License v3.0
+*/
+
+background(186, 145, 20); // wooden table
+ellipse(200, 200, 350, 350); // plate
+ellipse(200, 200, 300, 300); 
+
+// A Cheese
+fill(253, 165, 15);
+triangle(90, 180, 180, 180, 127, 85);
+fill(255, 255, 255);
+ellipse(130,120,20,20);
+ellipse(115,160,20,20);
+ellipse(151,160,20,20);
+
+// An Apple
+fill(255, 8, 0);
+stroke(255, 0, 0);
+ellipse(250, 130, 80, 80);
+fill(11, 102, 35);
+stroke(11, 102, 70);
+triangle(270, 120, 300, 100, 270, 80);
