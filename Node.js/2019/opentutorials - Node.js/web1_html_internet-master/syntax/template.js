fbbbfe62470ee27ac3232dfee9f0efd8b5d4fb53
Dongwon Lee <wva3cdae@gmail.com>
2019-01-21 22:23:39 +0900

    Finished "Node.js - 8.JavaScript Grammer - Template Literal"

---
+++ Node.js/2019/opentutorials - Node.js/web1_html_internet-master/syntax/template.js
@@ -0,0 +1,16 @@
+//var name = 'Cellularhacker'
+var name = 'k8805';
+var letter = 'Dear '+name+'\n\
+\n\
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, '+name+' sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. '+name+' Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. '+name;
+console.log(letter);
+
+var a = '1';
+// 리터럴 : 정보를 표현하는 방법, 정보를 표현하는 기본(단위).
+// `` 템플릿 리터널의 시작과 끝을 알리는 문자.
+
+
+var letter = `Dear ${name}
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, ${name} sed do eiusmod tempor ${1+1} incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ${name} Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ${name}`;
+console.log(letter);
