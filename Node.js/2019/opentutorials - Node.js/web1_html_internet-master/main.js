b376e3fecde6b8dcc241859389ba7d7fb16f7ff9
Dongwon Lee <wva3cdae@gmail.com>
2019-01-21 23:49:37 +0900

    Finished "Node.js - 13.Create App - Build text using file"

--- Node.js/2019/opentutorials - Node.js/web1_html_internet-master/main.js
+++ Node.js/2019/opentutorials - Node.js/web1_html_internet-master/main.js
@@ -18,31 +18,31 @@ var app = http.createServer(function(request,response){
         return;
     }
     response.writeHead(200);
-    var template = `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ul>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ul>
-      <h2>${title}</h2>
-      <p>
-        Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language. Although most often used to set the visual style of web pages and user interfaces written in HTML and XHTML, the language can be applied to any XML document, including plain XML, SVG and XUL, and is applicable to rendering in speech, or on other media. Along with HTML and JavaScript, CSS is a cornerstone technology used by most websites to create visually engaging webpages, user interfaces for web applications, and user interfaces for many mobile applications.
-      </p>
-    </body>
-    </html>
-
+    fs.readFile(`data/${title}`, 'utf8', function(err, description){
+      var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ul>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ul>
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>
     `;
+
+    response.end(template);
+  });
     //console.log(__dirname + _url)
     //response.end(fs.readFileSync(__dirname + _url));
-    response.end(template);
     //response.end('cellularhacker : ' + url);
 
 });
