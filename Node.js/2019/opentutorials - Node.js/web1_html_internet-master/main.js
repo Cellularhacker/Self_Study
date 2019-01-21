1b9b0463de53e39e57b47145735820879a77380b
Dongwon Lee <wva3cdae@gmail.com>
2019-01-21 23:21:17 +0900

    Finishied "Node.js - 11.Create App - Create Dynamic Web Page"

--- Node.js/2019/opentutorials - Node.js/web1_html_internet-master/main.js
+++ Node.js/2019/opentutorials - Node.js/web1_html_internet-master/main.js
@@ -5,9 +5,12 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-        console.log(queryData.id);
+    var title = queryData.id;
+
+    console.log(queryData.id);
+
     if(_url == '/'){
-      _url = '/index.html';
+      title = 'Welcome';
     }
     if(_url == '/favicon.ico'){
         response.writeHead(404);
@@ -15,9 +18,31 @@ var app = http.createServer(function(request,response){
         return;
     }
     response.writeHead(200);
-    console.log(__dirname + _url)
+    var template = `
+    <!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      <ul>
+        <li><a href="/?id=HTML">HTML</a></li>
+        <li><a href="/?id=CSS">CSS</a></li>
+        <li><a href="/?id=JavaScript">JavaScript</a></li>
+      </ul>
+      <h2>${title}</h2>
+      <p>
+        Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language. Although most often used to set the visual style of web pages and user interfaces written in HTML and XHTML, the language can be applied to any XML document, including plain XML, SVG and XUL, and is applicable to rendering in speech, or on other media. Along with HTML and JavaScript, CSS is a cornerstone technology used by most websites to create visually engaging webpages, user interfaces for web applications, and user interfaces for many mobile applications.
+      </p>
+    </body>
+    </html>
+
+    `;
+    //console.log(__dirname + _url)
     //response.end(fs.readFileSync(__dirname + _url));
-    response.end(queryData.id);
+    response.end(template);
     //response.end('cellularhacker : ' + url);
 
 });
