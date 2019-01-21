f4893bb5fccb75c55eb70706593c116f49622900
Dongwon Lee <wva3cdae@gmail.com>
2019-01-22 02:25:57 +0900

    Finished "Node.js - 25.3.JavaScript - Outputs of Function"

--- Node.js/2019/opentutorials - Node.js/web1_html_internet-master/syntax/function2.js
+++ Node.js/2019/opentutorials - Node.js/web1_html_internet-master/syntax/function2.js
@@ -1,8 +1,10 @@
-console.log(Math.round(1.6)); //2
-console.log(Math.round(1.4)); //1
+console.log(Math.round(1.6));
+console.log(Math.round(1.4));
 
-function sum(first, second){ // parameter
-  console.log(first+second);
+function sum(first, second) { // parameter
+  console.log('a');
+  return first+second;
+  console.log('b');
 }
 
-sum(2,4); // argument
+console.log(sum(2,4)); // argument
