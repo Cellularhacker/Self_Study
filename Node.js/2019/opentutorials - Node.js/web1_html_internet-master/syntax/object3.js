7cb840caaa8fabe7d62abcfeb39a3bf09a94097d
Dongwon Lee <wva3cdae@gmail.com>
2019-01-23 02:34:25 +0900

    Finished "Node.js - 42.JavaScript - Object - Object as a box storing Data & Processing Solution"

---
+++ Node.js/2019/opentutorials - Node.js/web1_html_internet-master/syntax/object3.js
@@ -0,0 +1,34 @@
+//Object Oriented Programming
+var v1 = 'v1';
+// 10000 lines of codes...
+v1 = 'cellularhacker';
+var v1 = 'v2';
+
+
+var q = {
+  v1:'v1',
+  v2:'v2',
+  f1:function(){
+    console.log(this.v1);
+  },
+  f2:function (){
+    console.log(this.v2);
+  }
+}
+
+/*
+function f1(){
+  console.log(o.v1);
+}
+
+function f1(){
+
+}
+
+function f2(){
+  console.log(o.v2);
+}
+*/
+
+q.f1();
+q.f2();
