74d6b93c7afb21aa4cd92bc6b900fc0d7d9a5186
Dongwon Lee <wva3cdae@gmail.com>
2019-01-23 02:09:37 +0900

    Finished "Node.js - 39.JavaScript - Type of Object"

---
+++ Node.js/2019/opentutorials - Node.js/web1_html_internet-master/syntax/object.js
@@ -0,0 +1,9 @@
+var members = ['cellularhacker', 'k8805', 'hoya'];
+console.log(members[1]); //k8805
+
+var roles = {
+  'programmer' : 'cellularhacker',
+  'designer' : 'k8805',
+  'manager' : 'hoya'
+}
+console.log(roles.designer); //k8805
