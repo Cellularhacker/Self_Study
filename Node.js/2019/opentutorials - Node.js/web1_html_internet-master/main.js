be30cd1cd05ec33ae29ad11b941c2ccc1a9b8379
Dongwon Lee <wva3cdae@gmail.com>
2019-01-23 01:42:32 +0900

    Finished "Node.js - 35.Create an App - Modify an article - Send content"

--- Node.js/2019/opentutorials - Node.js/web1_html_internet-master/main.js
+++ Node.js/2019/opentutorials - Node.js/web1_html_internet-master/main.js
@@ -55,7 +55,7 @@ var app = http.createServer(function(request,response){
           fs.readdir('./data', function(error, filelist){
             var title = queryData.id;
             var list = templateList(filelist);  // filelist 출력
-            var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`,`<a href="/create">create</a> <a href="/update?=id=${title}">update</a>`); // template 출력
+            var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`,`<a href="/create">create</a> <a href="/update?id=${title}">update</a>`); // template 출력
 
             response.writeHead(200);
             response.end(template);
@@ -68,7 +68,7 @@ var app = http.createServer(function(request,response){
           var title = 'WEB - create';
           var list = templateList(filelist);  // filelist 출력
           var template = templateHTML(title, list, `
-            <form action="http://localhost:3000/create_process" method="post">
+            <form action="/create_process" method="post">
               <p><input type="text" name="title" placeholder="title"></p>
               <p>
                   <textarea rows="8" cols="80" name="description" placeholder="description"></textarea>
@@ -97,6 +97,30 @@ var app = http.createServer(function(request,response){
         fs.writeFile(`data/${title}`, description, 'utf8', function(err){
           response.writeHead(302, {Location: `/?id=${title}`});
           response.end();
+        });
+      });
+    } else if(pathname === '/update'){
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+        fs.readdir('./data', function(error, filelist){
+          var title = queryData.id;
+          var list = templateList(filelist);  // filelist 출력
+          var template = templateHTML(title, list,
+            `
+            <form action="/update_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <p>
+                  <textarea rows="8" cols="80" name="description" placeholder="description">${description}</textarea>
+              </p>
+              <p>
+                  <input type="submit">
+              </p>
+            </form>
+            `,
+            `<a href="/create">create</a> <a href="/update?=id=${title}">update</a>`); // template 출력
+
+          response.writeHead(200);
+          response.end(template);
         });
       });
     } else {
